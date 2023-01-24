de9b93c1fc19f884971bd1a19768a22d5291fc67
Семён <sem.pakhomov2007@yandex.ru>
2023-01-24 17:30:16 +0300

    1 task

--- main.py
+++ main.py
@@ -1,31 +1,71 @@
 import pygame
 import requests
 import os
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.Qt import QLineEdit, QLabel, QPushButton
+import sys
 
-ll = input().split()
 
-spn = input()
+class Input(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-api_server = "http://static-maps.yandex.ru/1.x/"
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Введите координаты')
+        l = QLabel(self)
+        l.setText("Долгота: ")
+        l.move(10, 70)
+        self.line_ed_l1 = QLineEdit(self)
+        self.line_ed_l1.move(70, 70)
 
-params = {
-    "ll": ",".join([ll[0], ll[1]]),
-    "spn": ",".join([spn, spn]),
-    "l": "map"
-}
+        l = QLabel(self)
+        l.setText("Широта: ")
+        l.move(10, 140)
+        self.line_ed_l2 = QLineEdit(self)
+        self.line_ed_l2.move(70, 140)
 
-response = requests.get(api_server, params=params)
+        s = QLabel(self)
+        s.setText("Размер: ")
+        s.move(10, 210)
+        self.s = QLineEdit(self)
+        self.s.move(70, 210)
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+        self.bt = QPushButton(self)
+        self.bt.clicked.connect(self.map)
+        self.bt.move(240, 140)
 
-os.remove(map_file)
+    def map(self):
+        ll = self.line_ed_l1.text(), self.line_ed_l2.text()
+        spn = self.s.text()
+        api_server = "http://static-maps.yandex.ru/1.x/"
+
+        params = {
+            "ll": ",".join([ll[0], ll[1]]),
+            "spn": ",".join([spn, spn]),
+            "l": "map"
+        }
+
+        response = requests.get(api_server, params=params)
+
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+        pygame.init()
+        screen = pygame.display.set_mode((600, 450))
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+        while pygame.event.wait().type != pygame.QUIT:
+            pass
+        pygame.quit()
+
+        os.remove(map_file)
+
+
+app = QApplication(sys.argv)
+ex = Input()
+ex.show()
+sys.exit(app.exec())
+
 # пример ввода ll - 37.530887 55.703118 | spn - 0.002
